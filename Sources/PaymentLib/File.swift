2e402f78a5b53e43df252fc2e7c12adc4c577c57
pierostud <piero.amato66@gmail.com>
2020-11-03 16:00:20 +0100

    aggiunta classe

---
+++ Sources/PaymentLib/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by PieroAdmin on 03/11/2020.
+//
+
+import Foundation
+
+
+public struct Appo {
+    public func getName() -> String {
+        return "nome"
+    }
+}
